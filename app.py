d806304dff96f4341b9d4ce28e575a6ef1649c8a
archit009 <archithinge9@gmail.com>
2021-11-11 16:47:57 +0530

    runserver

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello I am Archit, This is my capstone project submission."
-app.run(host="0.0.0.0", port=8080, debug=True)
+app.run(host="0.0.0.0", port=80, debug=True)
